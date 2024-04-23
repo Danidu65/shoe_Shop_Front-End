520779687b2b6a66fcc7a6a5d70db94cb6e88961
Danidu Viraj <daniduijse65@Gmail.com>
2024-04-23 20:31:07 +0530

    initial commit

---
+++ Admin Dashboard #01/index.js
@@ -0,0 +1,47 @@
+const menuOpen = document.getElementById('menu-open');
+const menuClose = document.getElementById('menu-close');
+const sideBar = document.querySelector('.container .left-section');
+const sidebarItems = document.querySelectorAll('.container .left-section .sidebar .item');
+
+menuOpen.addEventListener('click', () => {
+    sideBar.style.top = '0';
+});
+
+menuClose.addEventListener('click', () => {
+    sideBar.style.top = '-60vh';
+});
+
+let activeItem = sidebarItems[0];
+
+sidebarItems.forEach(element => {
+    element.addEventListener('click', () => {
+        if (activeItem) {
+            activeItem.removeAttribute('id');
+        }
+
+        element.setAttribute('id', 'active');
+        activeItem = element;
+
+    });
+});
+
+const toggler = document.getElementById('theme-toggle');
+
+toggler.addEventListener('change', function () {
+    if (this.checked) {
+        document.body.classList.add('dark');
+    } else {
+        document.body.classList.remove('dark');
+    }
+});
+let circle = document.querySelector(".color-option");
+
+circle.addEventListener("click", (e)=>{
+    let target = e.target;
+    if(target.classList.contains("circle")){
+        circle.querySelector(".active").classList.remove("active");
+        target.classList.add("active");
+        document.querySelector(".main-images .active").classList.remove("active");
+        document.querySelector(`.main-images .${target.id}`).classList.add("active");
+    }
+});
